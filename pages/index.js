425a829866e0a5479bbe29c9c2659b4015c52302
Surachai <46965738+celestialcodeTH@users.noreply.github.com>
2020-09-30 11:43:03 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -33,19 +33,19 @@ export default function Home() {
           description: "Open Graph Description",
           images: [
             {
-              url: "https://www.example.ie/og-image-01.jpg",
+              url: "/opengraph.png",
               width: 800,
               height: 600,
               alt: "Og Image Alt",
             },
             {
-              url: "https://www.example.ie/og-image-02.jpg",
+              url: "/opengraph.png",
               width: 900,
               height: 800,
               alt: "Og Image Alt Second",
             },
-            { url: "https://www.example.ie/og-image-03.jpg" },
-            { url: "https://www.example.ie/og-image-04.jpg" },
+            { url: "/opengraph.png" },
+            { url: "/opengraph.png" },
           ],
           site_name: "SiteName",
         }}
--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -2,13 +2,14 @@ import React from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
+import { NextSeo } from "next-seo";
 
 const Product = () => {
   const route = useRouter();
   const productId = route.query.productId;
   return (
     <div className={styles.container}>
-      <Head>
+      {/* <Head>
         <meta name="msapplication-TileColor" content="#000000" />
         <meta
           name="msapplication-config"
@@ -25,7 +26,39 @@ const Product = () => {
           content="ให้เช่าด่วน ทาวน์โฮม อินดี้ บางนา อยู่ใกล้ เมกะบางนา บ้านสวยน่าอยู่มากๆ ซอยนี้มีแต่บ้านหรูๆ อยู่ซอยบางแก้ว อยู่ติดเมกะบางนาเลย ทำเลดีจริงๆ ขึ้นทางด่วนได้เลย"
         />
         <meta property="og:image" content="/opengraph.png" />
-      </Head>
+      </Head> */}
+      <NextSeo
+        title="Using More of Config"
+        description={`This example uses more of the available config options. ${productId}`}
+        canonical="https://www.canonical.ie/"
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: `Open Graph Title ${productId}`,
+          description: `Open Graph Description ${productId}`,
+          images: [
+            {
+              url: "/opengraph.png",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+            },
+            {
+              url: "/opengraph.png",
+              width: 900,
+              height: 800,
+              alt: "Og Image Alt Second",
+            },
+            { url: "/opengraph.png" },
+            { url: "/opengraph.png" },
+          ],
+          site_name: "SiteName",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
       <h1>Product | {productId}</h1>
     </div>
   );
